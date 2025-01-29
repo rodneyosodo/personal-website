3ebbcc7dc0e27d570e2c870967c57d6bd1f35873
Rodney Osodo <socials@rodneyosodo.com>
2025-01-29 21:17:38 +0300

    fix: make posthog not to be tracked by ad blockers
    Signed-off-by: Rodney Osodo <socials@rodneyosodo.com>

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,39 @@ import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  experimental:{
-    taint: true
-  }
+  experimental: {
+    taint: true,
+  },
+  async rewrites() {
+    const postHogBaseURL = process.env.NEXT_PUBLIC_POSTHOG_HOST;
+    if (!postHogBaseURL) {
+      return [];
+    }
+
+    const assetPath =
+      postHogBaseURL === "https://us.i.posthog.com"
+        ? "https://us-assets.i.posthog.com"
+        : postHogBaseURL === "https://eu.i.posthog.com"
+          ? "https://eu-assets.i.posthog.com"
+          : process.env.NEXT_PUBLIC_POSTHOG_ASSET_HOST || "";
+
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: assetPath + "/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: postHogBaseURL + "/:path*",
+      },
+      {
+        source: "/ingest/decide",
+        destination: postHogBaseURL + "/decide",
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 const withMDX = createMDX({});
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import posthog from "posthog-js";
-// biome-ignore lint/style/useNamingConvention: 
 import { PostHogProvider as PHProvider } from "posthog-js/react";
 import { useEffect } from "react";
 import SuspendedPostHogPageView from "@/app/posthog-page-view";
@@ -10,11 +9,9 @@ import type React from "react";
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-      // biome-ignore lint/style/useNamingConvention: This is used by PostHog
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST as string,
-      // biome-ignore lint/style/useNamingConvention: This is used by PostHog
+      api_host: "/ingest",
+      ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST as string,
       capture_pageview: false,
-      // biome-ignore lint/style/useNamingConvention: This is used by PostHog
       capture_pageleave: true,
     });
   }, []);
