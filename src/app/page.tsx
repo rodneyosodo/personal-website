673aa0acf09ff0fa6485ec3ac5d81c1e47565928
Rodney Osodo <socials@rodneyosodo.com>
2026-02-16 22:22:32 +0300

    fix(page): correct heading class to text-xl
    Signed-off-by: Rodney Osodo <socials@rodneyosodo.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           />
         </div>
         <h1 className="text-4xl font-bold mb-6">Rodney Osodo</h1>
-        <h2 className="text-1xl mb-2">
+        <h2 className="text-xl mb-2">
           Mechatronics Engineer | Community Builder | Software Engineer
         </h2>
         <div className="max-w-2xl text-muted-foreground">
