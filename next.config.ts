76538202ca8ba36fa470e3d5fd3850e0baa6f9c2
Rodney Osodo <socials@rodneyosodo.com>
2026-05-05 17:46:35 +0300

    feat(next): add image formats and deviceSizes
    Signed-off-by: Rodney Osodo <socials@rodneyosodo.com>

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   experimental: {
     taint: true,
