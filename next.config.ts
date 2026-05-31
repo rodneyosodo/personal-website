5dbea10e439c2a4d38dc93ecb796f3464fd33eda
Rodney Osodo <socials@rodneyosodo.com>
2026-05-31 20:57:46 +0300

    feat(next): allow r2.rodneyosodo.com as remote image host
    Signed-off-by: Rodney Osodo <socials@rodneyosodo.com>

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    remotePatterns: [{ protocol: "https", hostname: "r2.rodneyosodo.com" }],
   },
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   experimental: {
